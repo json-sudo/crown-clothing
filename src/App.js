5b8f681ff4c73ef9966e85bb89a195a6cd4c1f64
jason.omemu <jasonjasyno50@gmail.com>
2019-08-22 18:19:25 +0100

    implementing redirect to home on signin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Switch, Route} from 'react-router-dom';
-import { connect } from 'react-redux';
+import { Switch, Route, Redirect} from 'react-router-dom';
 import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.page';
-import './App.css';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.action';
+import { connect } from 'react-redux';
+import './App.css';
 
 class App extends React.Component {
   unSuscribeFromAuth = null;
@@ -41,15 +41,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)}  />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps(setCurrentUser))(App);
+export default connect(mapStateToProps, mapDispatchToProps(setCurrentUser))(App);
