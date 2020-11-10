f15c608be4b501ef6fb74a0aa0b4e384d1b47a79
Jason <jasonjasyno50@gmail.com>
2020-11-10 10:49:44 +0100

    more fixes

--- client/src/components/drawer/drawer.component.jsx
+++ client/src/components/drawer/drawer.component.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import CrownLogo from '../../assets/crown-logo.svg'
 
 import DrawerContext from '../../contexts/drawer.context'
 
